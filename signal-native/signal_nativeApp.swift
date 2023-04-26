841b90a8d911b23a932a6f308e75947b88a3fd1f
jakubbibro <jakub.bibro@gravity9.com>
2023-04-26 18:04:39 +0200

    Initial Commit

---
+++ signal-native/signal_nativeApp.swift
@@ -0,0 +1,17 @@
+//
+//  signal_nativeApp.swift
+//  signal-native
+//
+//  Created by Jakub Bibro on 26/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct signal_nativeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
